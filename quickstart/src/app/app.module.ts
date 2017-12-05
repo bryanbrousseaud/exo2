5de8b2349befb668971b2cc934f003a1a3e0ea45
orianaD <kira2616@gmail.com>
2017-12-05 12:50:08 +0100

    ajout partie 4

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -3,6 +3,8 @@
     import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
      
     import { AppComponent }  from './app.component';
+
+    import { HeroDetailComponent } from './hero-detail.component';
      
     @NgModule({
       imports: [
@@ -10,7 +12,8 @@
         FormsModule // <-- import the FormsModule before binding with [(ngModel)]
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeroDetailComponent
       ],
       bootstrap: [ AppComponent ]
     })
